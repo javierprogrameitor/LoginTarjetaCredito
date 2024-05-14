a47d2c916e52aeee034b1a9d05d1f69c3c48a941
javierprogrameitor <102185504+javierprogrameitor@users.noreply.github.com>
2024-05-14 14:02:58 +0200

    Change for recovery
    Fixed some issues

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 let inputName = document.getElementById("name");
 let inputNum = document.getElementById("numero");
 let inputFech = document.getElementById("fecha");
@@ -10,115 +9,70 @@ const NUM_INVALID = "El numero no coincide con su entidad";
 const FECH_INVALID = "Fecha Incorrecta";
 const CVV_INVALID = "Solo 3 dígitos";
 
-
-inputName.addEventListener("blur", vName);
-inputNum.addEventListener("blur", vNum);
-inputFech.addEventListener("blur", validaFech);
-inputCvv.addEventListener("blur", vCvv);
+inputName.addEventListener("input", vName);
+inputNum.addEventListener("input", vNum);
+inputFech.addEventListener("input", validaFech);
+inputCvv.addEventListener("input", vCvv);
 
 let fechV = false;
 let ccV = false;
 
-
 function check() {
     if (fechV && ccV) {
         submitButton.classList.remove("notAvailable");
+        submitButton.disabled = false;
     } else {
-        submitButton.classList = "notAvailable";
+        submitButton.classList.add("notAvailable");
+        submitButton.disabled = true;
     }
 }
 
 function vName() {
-
-    if (inputName === numVV || inputName === numVM || inputName === numVA) {
-
-        inputName.parentNode.getElementsByTagName("small")[0].innerHTML = ENTITY_INVALID;
-
-    } else {
-        inputName.parentNode.getElementsByTagName("small")[0].innerHTML = "";
-    }
-
-
+    let regexName = /^(MasterCard|Visa|American Express)$/i;
+    let nameV = regexName.test(inputName.value);
+    inputName.className = nameV ? "success" : "error";
+    inputName.parentNode.getElementsByTagName("small")[0].innerHTML = nameV ? "" : ENTITY_INVALID;
+    check();
 }
 
 function vNum() {
+    let regexVisa = /^4[0-9]{12}(?:[0-9]{3})?$/;  //Visa
+    let regexMaster = /^5[1-5][0-9]{14}$/;  //MasterCard
+    let regexAmerican = /^3[47][0-9]{13}$/; //American Express
 
-    let numVV = "Visa";
-    let numVM = "MasterCard";
-    let numVA = "AmericanExpess";
-
-    let regexVisa = /^4\d{3}-?\d{4}-?\d{4}-?\d{4}$/;  //Visa
-    let regexMaster = /^5[1-5]\d{2}-?\d{4}-?\d{4}-?\d{4}$/;  //MasterCard
-    let regexAmerican = /^3[47][0-9]{13}$/; //American
-
-    numVV = regexVisa.test(inputNum.value);
-    numVM = regexMaster.test(inputNum.value);
-    numVA = regexAmerican.test(inputNum.value);
-
-    inputNum.className = numVV ? "success" : "error";
-    inputNum.className = numVM ? "success" : "error";
-    inputNum.className = numVA ? "success" : "error";
-
-    if (!numVV && !numVM && !numVA) {
-        // Obtenemos la etiqueta <small> del div al que pertenece el input
-        inputNum.parentNode.getElementsByTagName("small")[0].innerHTML = "";
-    } else {
-        inputNum.parentNode.getElementsByTagName("small")[0].innerHTML = NUM_INVALID;
+    let numV = false;
+    if (inputName.value.toLowerCase() === "visa") {
+        numV = regexVisa.test(inputNum.value);
+    } else if (inputName.value.toLowerCase() === "mastercard") {
+        numV = regexMaster.test(inputNum.value);
+    } else if (inputName.value.toLowerCase() === "american express") {
+        numV = regexAmerican.test(inputNum.value);
     }
-// let reg = new RegExp("(((0[123456789]|10|11|12)/(([1][9][0-9][0-9])|([2][0-9][0-9][0-9]))))");
 
+    inputNum.className = numV ? "success" : "error";
+    inputNum.parentNode.getElementsByTagName("small")[0].innerHTML = numV ? "" : NUM_INVALID;
+    check();
 }
 
 function validaFech() {
-
-  
-    let pater = /'^(0[1-9]|1[0-2])\/([1-9][0-9])$'/;
-
-    fechV = pater.test(inputFech.value);
+    let regexFech = /^(0[1-9]|1[0-2])\/([0-9]{2})$/;
+    fechV = regexFech.test(inputFech.value);
     inputFech.className = fechV ? "success" : "error";
-
-    if (!fechV) {
-        inputFech.parentNode.getElementsByTagName("small")[0].innerHTML = "";
-    } else {
-        inputFech.parentNode.getElementsByTagName("small")[0].innerHTML = FECH_INVALID
-    }
+    inputFech.parentNode.getElementsByTagName("small")[0].innerHTML = fechV ? "" : FECH_INVALID;
     check();
-
 }
 
 function vCvv() {
-
-    let patron = /'^[0-9]{3}$'/;
-
-    ccV = patron.test(inputCvv.value);
-    inputCvv.className = ccV ? "success" : "error"; 
-
-
-    if (!ccV) {
-        inputCvv.parentNode.getElementsByTagName("small")[0].innerHTML = "";
-    } else {
-        inputCvv.parentNode.getElementsByTagName("small")[0].innerHTML = CVV_INVALID
-
-    }
+    let regexCvv = /^[0-9]{3}$/;
+    ccV = regexCvv.test(inputCvv.value);
+    inputCvv.className = ccV ? "success" : "error";
+    inputCvv.parentNode.getElementsByTagName("small")[0].innerHTML = ccV ? "" : CVV_INVALID;
     check();
 }
 
-form.addEventListener('submit', event => {
-    /*
-    Para detener el envío del formulario, llamar al método preventDefault() del objeto de evento
-    dentro del controlador de eventos de envío de esta manera:
-    */
+submitButton.addEventListener('click', event => {
     event.preventDefault();
-  
-    /*
-    Para enviar el formulario tras validarlo, llamariamos al método submit() del objeto del
-    formulario:
-    */
-    form.submit();
-  
-    // Sólo probamos que funciona con un alert()
-    alert("Enviado");
-  
-  
-  });
-  
+    if (!submitButton.disabled) {
+        alert("Enviado");
+    }
+});
